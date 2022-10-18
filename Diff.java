380c88facc93180f60454c2fd07d127b1c7bd47b
Devang Kale <devangkale3@gmail.com>
2022-10-18 18:55:25 -0400

    Changes in Diff.java

--- Diff.java
+++ Diff.java
@@ -3,6 +3,6 @@ public class Diff {
         int a = 5;
         int b = 6;
 
-        System.out.println("The Difference is" + (a-b));
+        System.out.println("The difference is" + (a-b));
     }
 }
